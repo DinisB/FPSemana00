1f541ec77cc4202c0f5d50a31088bac3f7eff42e
DinisB <dinisbarroso.dev@gmail.com>
2024-11-04 15:34:50 +0000

    Exercicio update

--- Ex1.py
+++ Ex1.py
@@ -6,8 +6,7 @@ room_desc = [
 [ "U", "V", "X", "Y", "Z" ],
 ]
 
-room_exits = [(False, False, True, False),
- (False, True, False, False), (False, True, True, True), (False, True, True, True), (False, False, True, True) ],
+room_exits = [(False, False, True, False), (False, True, False, False), (False, True, True, True), (False, True, True, True), (False, False, True, True) ],
 [(True, False, True, False), (False, True, True, False), (True, True, True, True), (True, False, False, True), (True, False, True, False) ] ,
 [(True, False, True, False), (True, False, False, False), (True, False, True, False), (False, True, False, False), (True, False, True, True) ] ,
 [(True, False, True, False), (False, True, True, False), (True, True, False, True), (False, True, False, True), (True, False, True, False) ],
